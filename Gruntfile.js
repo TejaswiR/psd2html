d0874c69fb349b7099165ba14739a1ca3c83bcd8
TejaswiR <tejaswi.ramisetty@gmail.com>
2015-08-13 22:55:31 +0530

    Reorganising static files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,22 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-  sass: {                              // Task 
-    dist: {                            // Target 
-      files: {                         // Dictionary of files 
-        'styles.css': 'styles.scss',       // 'destination': 'source' 
-        }
-     }
-  },
-  watch: {
-   css: {
-    files: ['*.sass'],
-    tasks: ['sass'],
-      }
- 
-    }
-});
+          grunt.initConfig({
+              sass: {                              // Task 
+                 dist: {                            // Target 
+                   files: {                         // Dictionary of files 
+                      'assets/stylesheets/common.css': 'assets/stylesheets/src/common.scss',      
+                      'assets/stylesheets/land.css': 'assets/stylesheets/src/land.scss',      
+                      }
+                    }
+                 },
+                 watch:   {
+                      css: {
+                     files: ['assets/stylesheets/src/*.scss'],
+                   tasks: ['sass'],
+                 }
+              }
+         });
+    
+
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
