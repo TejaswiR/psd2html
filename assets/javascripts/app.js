0c1c4fa3f1adbafb6e31b4448ac9167a783c5bec
TejaswiR <tejaswi.ramisetty@gmail.com>
2015-08-15 19:10:24 +0530

    done all changes

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -7,8 +7,11 @@ $('header nav').affix({
 
 $('body').scrollspy({
 
- target: '#nav-link-container' 
- offset: 100
+ target: '#nav-link-container' ,
+ offset: 55
+});
 
-})
+var email_components =["hello" , "@" , "example" , "." , "com" ];
+var  email = email_components . join(" ");
+$('#email-link').html (email).attr ('href',  'mailto:' +email);
 });
